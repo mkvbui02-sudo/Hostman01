a971b540dcc777714c39b069f0e4a6a5b29df4d6
mkvbui02-sudo <mkvbui02@gmail.com>
2025-09-18 22:37:18 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "23557588"))
+API_HASH = environ.get("API_HASH", "7ed52f38f74fe85ebc2e5e285051c3b6")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8237804548:AAEKaoUNg-sEFGqu4FPvyAQzi5W52dYcu2k")
 
-OWNER = int(environ.get("OWNER", "5680454765"))
+OWNER = int(environ.get("OWNER", "2084001270"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
